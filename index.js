6d19ebda0b6d19df44ea0cf51cb951b7eb4a965a
bhadreshce <bhadreshce17@gmail.com>
2023-04-07 09:13:11 +0530

    added daily lecture

---
+++ index.js
@@ -0,0 +1,49 @@
+// variable
+// let
+var price = 1212.1;
+// number
+
+// string
+var name = 'bhadresh';
+// boolean 
+var check = false
+// null 
+var balance = null;
+
+// undefined
+
+var qtn = undefined; 
+
+ 
+//array
+
+// array is collection multiple values 1 or more 
+// index array 
+  var std = ['jay','jignesh',null,undefined,12344,['bhavesh','rahul','nilesh'] ]
+//multidimention array 
+
+
+// object
+
+//array of object 
+var std = [
+  { name: 'jignesh', mark: 50, subject: 'maths' },
+  { name: 'jay', mark: 50, subject: 'maths' },
+  { name: 'jiten', mark: 50, subject: 'maths' },
+  { name: 'jay', mark: 50, subject: 'maths' },
+  {name:'jignesh',mark:50,subject:['maths','physics'] }
+]
+   
+
+console.log(std[4].subject[1]);
+//symbol
+// bigint
+
+
+
+// vairable : for store value in our program
+//  name will be start from _ or alphabetic value
+// name not will start from number
+//   you can add numebr in beetween first latter to last latter
+//  you cant use symbolic value  in variable name
+
---
+++ op.js
@@ -0,0 +1,35 @@
+// arithmetic + ,- ,%,/
+// var a = 2
+// var b = 9
+// var c = 7
+
+// var d =  a+b*c/10
+
+//   var F = 0
+// C = [(F-32)*5]/9
+// var d = 10%2
+// var d = 10/5
+// console.log(C);
+// LOGICAL OPERATOR  && ||   !
+// var a = false
+// var b = false
+//   console.log(!a || b);
+// relational operator , >,<,<=,>=,==,===,!=, !==
+
+var a = 5
+var b = '5'
+var c = true
+var arry = [1, 2, 3, 4, 5, 6]
+var obj = undefined
+// typof m
+// 
+// true  && true  = true
+// false && true  = false
+// false && false = false
+
+// false || false  = false 
+// false || true = true
+// true || true = true 
+console.log(typeof obj);
+ 
+//
