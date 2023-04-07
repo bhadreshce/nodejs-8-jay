c14ef8d6cd852bc204401b6cc898176387223ffd
bhadreshce <bhadreshce17@gmail.com>
2023-04-07 09:54:32 +0530

    added new file

--- op.js
+++ op.js
@@ -16,20 +16,32 @@
 //   console.log(!a || b);
 // relational operator , >,<,<=,>=,==,===,!=, !==
 
-var a = 5
-var b = '5'
-var c = true
-var arry = [1, 2, 3, 4, 5, 6]
-var obj = undefined
+// var a = 5
+// var b = '5'
+// var c = true
+// var arry = [1, 2, 3, 4, 5, 6]
+// var obj = undefined
 // typof m
-// 
+//
 // true  && true  = true
 // false && true  = false
 // false && false = false
 
-// false || false  = false 
+// false || false  = false
 // false || true = true
-// true || true = true 
-console.log(typeof obj);
+// true || true = true
+// console.log(typeof obj);
  
-//
+//bitwise  <<  >> 
+// var b = 5
+
+//ternory operator
+
+// (condition)? if true : if false
+
+var c = (5 < 7)? '5': '7';
+console.log(c);
+// console.log(5>>1);
+
+  
+
