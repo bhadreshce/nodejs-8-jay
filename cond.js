a06696289483166cfdae5345896bbe5a6ced308d
bhadreshce <bhadreshce17@gmail.com>
2023-04-14 08:24:50 +0530

    condition

---
+++ cond.js
@@ -0,0 +1,113 @@
+//condition
+// if
+//if else
+//if else if
+// nested if else
+// swich case
+
+// var mark = 40;
+// if (50 <= mark) {
+//     console.log('science');
+// } else {
+//     console.log('commerce');
+// }
+
+// var a = 6;
+
+// if (a % 2 == 0) {
+//     console.log('this is even number');
+// } else {
+//     console.log('this is odd number');
+// }
+
+// var mark = 40
+// if (mark > 50) {
+//                  console.log('congratulation your admition succesfull ');
+//     if (mark > 55) {
+//         console.log('class A');
+   
+
+//     } else {
+//         console.log('class b');
+//     }
+// } else {
+//     console.log('you cant take admition ');
+// }
+
+// if else if
+
+// var mark = 80;
+// if (mark < 35) {
+//     console.log('grade E');
+// } else if (mark < 40) {
+//     console.log('grade d');
+// } else if (mark < 50) {
+//     console.log('grade c');
+// } else if (mark < 70) {
+//     console.log('grade b');
+// } else if (mark < 90) {
+//     console.log('grade a1');
+// }
+
+//swich case
+
+// var fruit = 'mango'
+
+// switch (fruit) {
+//     case 'banana':
+//         console.log('this is a banana');
+        
+//         break;
+    
+//    case 'apple':
+//         console.log('this is a apple');
+        
+//         break;
+
+//     default:
+//         console.log('fruit not found');
+//         break;
+// }
+
+//function
+// function sum(a,b) {
+//     console.log(a+b);
+// }
+// sum(5, 7)
+
+// sum(5, 10)
+
+// sum(10, 20)
+
+//with  parameter , without return
+
+// function minuteTosec(minute) {
+//       return minute*60
+// }
+
+
+// var a = minuteTosec(1)
+
+// console.log(a);
+//  var b = minuteTosec(2)
+// console.log(b);
+ // with parameter , with return
+
+// function divide() {
+//     console.log(1/2);
+// }
+
+// divide()
+
+// without para , without return
+
+// function modulo() {
+//       return 10%2
+// }
+
+// console.log(modulo());
+
+// with retun , without parameter
+
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(typeof obj);
+
